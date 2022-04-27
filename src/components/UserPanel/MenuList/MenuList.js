dab979a5cf1db3e52c8058460d490f12519cc6a2
SameerCrestha <ddav.crestha@gmail.com>
2022-04-27 21:37:05 +0545

    Fixed menuList bug

--- src/components/UserPanel/MenuList/MenuList.js
+++ src/components/UserPanel/MenuList/MenuList.js
@@ -4,55 +4,62 @@ import './MenuList.css';
 import { Ring } from 'react-awesome-spinners';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function MenuList(props) {
-  const [orderList,setOrderList]=useState([]);
-  function Row({item}){
-    const [count,setCount]=useState(0);
-    const [confirmed,setConfirmed]=useState(false);
-    const [itemIndex,setItemIndex]=useState(null);
-    function confirmButtonHandler(){
-      setConfirmed(true);
-      let temp={food:item,quantity:count};
-      orderList.push(temp);
-      setItemIndex(orderList.indexOf(temp));
-      setOrderList(orderList);
-    }
-    useEffect(()=>{
-      count?setConfirmed(false):setConfirmed(true);
-    },[count])
-    useEffect(()=>{
-      //remove item from orderList if confirmed false
-      if(!confirmed) {
-        orderList.splice(itemIndex,1);
-        setOrderList(orderList);
-      }
-    },[confirmed,itemIndex]);
+function Row({item,orderList,setOrderList}){
+  const [count,setCount]=useState(0);
+  const [confirmed,setConfirmed]=useState(false);
+  function confirmButtonHandler(){
+    setConfirmed(true);
+    let temp={food:item,quantity:count};
+    setOrderList((prevOrderList)=>{
+      let newOrderList=[...prevOrderList,temp];
+      return newOrderList;
+    });
     
-    return (<tr key={item.food_id}>
-            <td>{item.food_name}</td>
-            <td>Rs {item.food_price}</td>
-            <div className='counter'>
-            <FontAwesomeIcon className='countButton' icon="fa-plus" onClick={()=>{setCount(count+1)}}/>
-            <div className='counterText'>{count}x</div>
-            <FontAwesomeIcon className='countButton' icon="fa-minus" onClick={()=>count?setCount(count-1):""}/>
-            <FontAwesomeIcon className={'countButton addButton '+(confirmed?"disable":"")} icon="fa-check" onClick={confirmButtonHandler}/>
-            </div>
-          </tr>)
   }
-  function Section({index,header,items}){
+  
+  useEffect(()=>{
+    if(count===0)setConfirmed(true)
+    else setConfirmed(false);
+  },[count])
+  useEffect(()=>{
+    //remove item from orderList if confirmed false
+    if(!confirmed ) {
+      setOrderList(prevOrderList=>{
+        if(prevOrderList.length!==0){
+          let newOrderList=prevOrderList.filter(el => el.food.food_id !== item.food_id)
+          return newOrderList;
+        }else return prevOrderList;
+      });
+    }
+  },[confirmed]);
+  
+  return (<tr key={item.food_id}>
+          <td>{item.food_name}</td>
+          <td>Rs {item.food_price}</td>
+          <div className='counter'>
+          <FontAwesomeIcon className='countButton' icon="fa-plus" onClick={()=>{setCount(count+1)}}/>
+          <div className='counterText'>{count}x</div>
+          <FontAwesomeIcon className='countButton' icon="fa-minus" onClick={()=>count?setCount(count-1):""}/>
+          <FontAwesomeIcon className={'countButton addButton '+(confirmed?"disable":"")} icon="fa-check" onClick={confirmButtonHandler}/>
+          </div>
+        </tr>)
+}
+function Section({index,header,items,orderList,setOrderList}){
   return(
     <>
       <thead><tr><td>{header}</td></tr></thead>
       <tbody>
         {items.map(el=>
-          <Row item={el}/>
+          <Row item={el} orderList={orderList} setOrderList={setOrderList}/>
         )}
       </tbody>
     </>
   );
 
   }
-  const[data,setData]=useState();//data format:[{header:"header",items:[{food_name:"momo",food_price:100},{}]},{}]
+function MenuList(props) {
+  const [orderList,setOrderList]=useState([]);
+  const[data,setData]=useState();//data format:[{header:"header",items:[{food_name:"momo",food_price:100},{}]},{}]  
   useEffect(()=>{
     const baseUrl=process.env.REACT_APP_BASE_URL;
     const foodPrefix=process.env.REACT_APP_FOOD_PREFIX;
@@ -92,7 +99,10 @@ function MenuList(props) {
    getCategories().then(categories=>populateData(categories)).then(data=>setData(data));
   },[])
   return(
-  !data?<div className='loadingDiv'><Ring/></div>:<Table className='userMenuTable' borderless hover>{data.map((el,index)=><Section index={index} header={el.header} items={el.items}/>)}</Table>
+  !data?<div className='loadingDiv'><Ring/></div>:
+  <Table className='userMenuTable' borderless hover>
+    {data.map((el,index)=><Section index={index} header={el.header} items={el.items} orderList={orderList} setOrderList={setOrderList}/>)}
+  </Table>
   );
 }
 
